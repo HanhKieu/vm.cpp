071cceaf80b9bf5faf08437758f45420458a8172
Hanh Kieu <hckieu@ucdavis.edu>
2015-07-27 16:50:09 -0700

    loop around fixed tester 20 works

--- vm.cpp
+++ vm.cpp
@@ -2,215 +2,178 @@
 #include <string>
 #include <bitset>
 #include <fstream>
-//banana
-
-
-// void printRam(ram,use);printRam(unsigned int ram[])
-// {
-// 	for(int i = 0; i < 3; i++){	
-// 		if(ram[i] != 0)
-// 			std::cout << std::hex << ram[i]<< "000" << " ";
-// 	}
-// 	if(ram[3] != 0)
-// 		std::cout << std::hex << ram[3] << "000";s
-// 	std::cout << std::endl;
-
-// }
-int ifInRam(unsigned int ram[], unsigned int nutsack){
-	for(int i = 0; i < 4; i++){	
-		if(ram[i] == nutsack)
-			return i;
-	}
-
-
-	return -1;
-
-
-
-}//yoooo
-int fullUseChecker(unsigned int use[]){
-	int full = 1;
-	for(int i = 0;i < 4; i++){
-		if(use[i] == 0){	
-			full = 0;
-			break;
-		}
-
-	}
-	return full;
-}
-
-void printRamUse(unsigned int ram[], unsigned int use[], int lineCounter, unsigned int useIndex, unsigned int nutsack)
-{
-	for(int i = 0; i < 3; i++){	
-		if(ram[i] != 0)
-			std::cout << std::hex << ram[i]<< "000" << " ";
-	}
-	if(ram[3] != 0)
-		std::cout << std::hex << ram[3] << "000";
-	std::cout << std::endl;
-	for(int i = 0; i < 3; i++){	
-			std::cout << use [i] << " ";
-	}
-	
-	std::cout << use[3];
-	std::cout << std::dec << " line: " << lineCounter << " Index is : " << useIndex ;
-	std::cout << std::hex << " was Trying to add: " << nutsack << std::endl;
-	
-}
-
 
 int main(){
+	int i,j;
+	/*Initialize virtual memory*/
+	unsigned int ram[4];//physical page 
+	unsigned int use[4];//use bit
 
-
-
-	unsigned int ram[4];
-	unsigned int use[4];//use_bit
-	for(int i = 0; i < (4); i++){
-		use[i] = 0;
+	for(i = 0; i < 4; i++){
 		ram[i] = 0;
+		use[i] = 0;
 	}
-
-
-	// unsigned int table[16][2];//i = virtual page number; table[i] = physical page number
-	// for(i=0; i < 16; i++)
-	// 	for(j = 0; j < 2; j++)
-	// 		table[i][j] = 0;
-
+		
 	/*page write and read*/
 
 	std::ifstream fs;
 	fs.open("test20.txt");
-	unsigned int info, nutsack, full = 0, useIndex = 0;
-	int counter = 0;
-	int lineCounter = 1;
+	unsigned int info, vm_page;
+	//unsigned int offset;
+	int counter = 1;
+	int write_pos = -1;
+	int stop = 0;
+	int match = 0;
+	int pointing = 1;
+	int fulluseChecker;
 
-	
-	while(fs >> std::hex >> info)
-	{
+	while(fs >> std::hex >> info){
+		stop = 0;
+		write_pos = -1;
+		match = 0;
 
-		// offset = info & 0xFFF;
-		nutsack	= (info & 0xFFFFF000) >> 12;
+		//offset = info & 0xFFF;
+		vm_page	= (info & 0xFFFFF000) >> 12;
 
-		if(counter > 15){
+		if(counter == 17)	//initial case
+		{
+			ram[0] = vm_page;
+			use[0] = 1;
+			std::cout << std::hex << ram[0] << "000" << std::endl;
+		}
 
+		if(counter > 17) //counter > 16
+		{
 
-			if(full == 0){
+			for(i = 0; i<4;i++)//looking for the same address
+			{
+				if(ram[i] == vm_page)
+				{
+					match = 1;
+					use[i] = 1;
+					//pointing++;
+					if(pointing == 4)
+						pointing = 0;
 
-				for(int i = 0; i < 4; i++){
-					if(ram[i] == 0){
-						ram[i] = nutsack;
-						full = 0;
-						if(fullUseChecker(ram))
-						{
-							full = 1;
-							useIndex = 0;
-							use[i] = 1;
-							
-							break;
-						}
-						use[i] = 1;
-						//printRamUse(ram,use,lineCounter));printRam(ram);
-						break;
+					// if(use[i] == 0)
+					// {
+					// 	use[i] = 1;
+					// }
+
+					// else	//use[i] == 1
+					// {
+					// 	use[i] = 1;
+					// 	if(i == pointing)
+					// 	pointing++;
+
+					// 	if(pointing == 4)
+					// 	pointing = 0;
+					// }					
+
+				}
+			}//for - check if its in ram
+
+			if(match == 0)//address not match
+			{
+				
+				while(stop != 1)//write by clock replacement
+				{	
+
+					if(use[pointing] == 0)
+					{
+			
+						write_pos = pointing;
+						pointing++;
+						ram[write_pos] = vm_page;
+						use[write_pos] = 1;
+						stop = 1;
 					}
-					full = 1;
-				}
-				printRamUse(ram,use,lineCounter, useIndex, nutsack);
-				if(full == 1){
-					for(int j = 0;j < 4; j++)
-						use[j] = 0;
 
-				}
-			}//if its not full find a place to put it
-
-
-
-			else if(full == 1){
-
-				for(int i = useIndex; i < 4; i++){
-
-					if(ifInRam(ram,nutsack) == -1){
-						if(use[i] == 0){
-							std::cout << "Use at current: 0";
-							std::cout << std::endl;
-							if(fullUseChecker(use))
+					else
+					{
+						use[pointing] = 0;
+						pointing++;
+						fulluseChecker = 1;
+						for(int i = 0; i < 4; i++){
+							if(use[i] == 0)
 							{
-								useIndex = 0;
-								for(int j = 0;j < 4; j++)
-									use[j] = 0;
-								std::cout << "went into fulluse checker";
-								std::cout << std::endl;
+								fulluseChecker = 0;
 								break;
 							}
-							use[i] = 1;
-							if(i == 3){
-								useIndex = 0;
-							}
-							else{
-								useIndex = i + 1;
-							}
-							
-							ram[i] = nutsack;
-						
-							//printRamUse(ram,use,lineCounter));
-							//printRam(ram);
-							break;
 						}
-						else if(use[i] == 1)
+			
+						if(fulluseChecker == 1)
 						{
-							if(fullUseChecker(use))
-							{
-								useIndex = 0;
-								i = -1;
-								for(int j = 0;j < 4; j++)
-									use[j] = 0;
-								std::cout << "went into fulluse checker for 1";
-								std::cout << std::endl;
-
-							}
-							else{
-								use[i] = 0; 
-								std::cout << "Use at current: 1";
-							std::cout << std::endl;
-
-							}
-							
+							write_pos = -1;//all lines are used
+							stop = 1;
 						}
-					}
-					else{
-						use[ifInRam(ram,nutsack)] = 1;
-					}
-
-					if(useIndex == 3 && !fullUseChecker(use)){
-						std::cout << " I looped";
-						std::cout << std::endl;
-						useIndex = -1;
+						// if(pointing == 4)
+						// 	stop = 1;
 					}
 
-				}//for 
-		
+					if(pointing == 4)
+						pointing = 0;
 
-						
-				
-					//printRamUse(ram,use,lineCounter));
-					//printRam(ram);
+				}//clock goes through to find where to write. If it writes somewhere, the clock stops.
+			
 
- 
-				printRamUse(ram,use,lineCounter, useIndex, nutsack);
+				if(write_pos == -1)//all lines are using
+				{	
+					//std::cout << "Banana ";
+					for(i = 0; i<4; i++)
+						use[i] = 0;
+					ram[0] = vm_page;
+					use[0] = 1;
+					pointing = 1;
+				}
 
-			}//start implementing clock replacement when everything is full
-	
-		//printRam(ram);
-		lineCounter++;
-		}//if counter
-		
+
+			}
+			/*------------print out result--------------*/
+			for(j=0 ; j<4; j++)	//print out the result
+			{
+				if(ram[j] != 0)
+				{
+					if(counter == 18)
+					{
+						if(j != 1)
+							std::cout << std::hex << ram[j] << "000 ";
+						else
+							std::cout << std::hex << ram[j] << "000";
+					}
+					else if(counter == 19)
+					{
+						if(j != 2)
+							std::cout << std::hex << ram[j] << "000 ";
+						else
+							std::cout << std::hex << ram[j] << "000";
+					}
+					else
+					{
+						if(j != 3)
+							std::cout << std::hex << ram[j] << "000 ";
+						else
+							std::cout << std::hex << ram[j] << "000";
+					}
+				}
+
+
+			}
+			
+			for(j=0;j<4;j++)
+				std::cout << " " << use[j] << " ";
+			
+			
+			
+			/*^^^^^^^^^^^^print out result^^^^^^^^^^^*/
+			//std::cout << " Was trying to add: " << vm_page << " current pointing: " << pointing;
+			std::cout << std::endl;
+		}
+
 		counter++;
-	}//while
+	}
 
 	fs.close();
 
 	return 0;
 }
-
-
-
